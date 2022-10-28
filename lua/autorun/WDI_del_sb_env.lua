8c0d8219464f81cc61051812ae608169fdef36a9
NovaAstral <mariofluddd@gmail.com>
2022-10-28 15:27:53 -0300

    Updated WDI_del_sb_env - Commented the prints so console isn't spammed - Potentially fixed entities not having gravity when spawned (But will likely break addons like TARDIS flight and float modes), might cause alot of lag too, especially with alot of entities - Potentially fixed needing to clean the map when the map starts by waiting 2 minutes before deleting the environments and disabling life support

--- lua/autorun/WDI_del_sb_env.lua
+++ lua/autorun/WDI_del_sb_env.lua
@@ -8,13 +8,13 @@ if(game.GetMap() ~= "gm_what_destiny_intended") then return end
 local function DelEnv()
     for k, e in ipairs(ents.FindByClass("logic_case")) do
         if(e:GetName() ~= "sgc_alarm_case") then --dont delete the logic_case that controls the sgc lockdown
-            print(e)
+            --print(e)
             e:Remove()
         end
     end
 
     for k, e in ipairs(ents.FindByClass("base_sb_planet*")) do
-        print(e)
+        --print(e)
         e:Remove()
     end
 
@@ -28,16 +28,29 @@ local function DelEnv()
     end
 
     RunConsoleCommand("CAF_Addon_Destruct", "Life Support")
+
+    timer.Create("GravEnable",1000,0,Grav())
+end
+
+local function Grav() --This is probably laggy
+    for k, e in ipairs(ents.FindByClass("*")) do
+        e:SetGravity(1)
+        local phys = e:GetPhysicsObject()
+        
+        if phys:IsValid() then
+            phys:EnableGravity(true)
+        end
+    end
 end
 
 hook.Add("Initialize", "DelEnv on Mapstart", function()
-    timer.Create("DelEnv Wait", 5, 1, function()
+    timer.Create("DelEnv Wait",120,1,function()
         DelEnv()
-        DelEnv() --run twice because it doesnt work properly if I dont and im too lazy rn to fix it
+        DelEnv() --run twice because it doesnt work properly if I dont and im too lazy to fix it
     end)
 end)
 
-hook.Add("PostCleanupMap", "CleanupReset", function()
+hook.Add("PostCleanupMap","CleanupReset",function()
     DelEnv()
-    DelEnv() --run twice because it doesnt work properly if I dont and im too lazy rn to fix it
+    DelEnv() --run twice because it doesnt work properly if I dont and im too lazy to fix it
 end)
