713768f353d0c66163ec90456459e9dead3e1eb3
NovaAstral <mariofluddd@gmail.com>
2025-02-02 03:48:42 -0400

    added fixbombs and getwep ulx functions - Added ulx fixbombs (!fixbombs / !fixgbombs), which deletes gb5_shockwave_ent, to prevent lua errors on older versions of gbombs 5 - Added ulx getwep (!getwep) which returns the weapon you're holding

--- lua/ulx/modules/sh/nmainfuns.lua
+++ lua/ulx/modules/sh/nmainfuns.lua
@@ -33,3 +33,23 @@ friction:addParam{type = ULib.cmds.PlayersArg}
 friction:addParam{type = ULib.cmds.NumArg, default = 1, hint = "Number Friction", min = 0, max = 10}
 friction:defaultAccess(ULib.ACCESS_SUPERADMIN)
 friction:help("Sets targets friction multiplier")
+
+local fixgbombs = ulx.command(category, "ulx fixbombs", function(call_ply) --Deletes the gbombs shockwave which causes lua errors on the version alot of people are using :v
+	if SERVER then
+		for k, v in ipairs(ents.FindByClass("gb5_shockwave_ent")) do v:Remove() end
+	end
+    
+	ulx.fancyLogAdmin(call_ply, "#A Fixed GBombs Error.")
+end, {"!fixbombs", "!fixgbombs"}, true)
+
+fixgbombs:defaultAccess(ULib.ACCESS_SUPERADMIN)
+fixgbombs:help("Fixes the GBombs error that happens when you use an outdated version of GBombs")
+
+local getwep = ulx.command(category, "ulx getwep", function(call_ply)
+	local wep =  call_ply:GetActiveWeapon()
+    
+	ULib.tsay(calling_ply,"You have the weapon: "..wep,true)
+end, {"!getwep"}, true)
+
+getwep:defaultAccess(ULib.ACCESS_SUPERADMIN)
+getwep:help("Prints your active weapon")
